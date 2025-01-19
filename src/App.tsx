212e2e43d16adb496cc8e934e6598bd29bdfa931
ArielNyan <arthur.rigotti@gmail.com>
2025-01-18 21:38:26 -0300

    adicionado sistema de zindex

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import Window from './applications/comp/Window'
+
+
 function App() {
+  const [zCounter, setZCounter] = useState<number>(1)
+  const bringTofront = () => {
+    setZCounter((prev)=>prev+1)
+    return zCounter
+  }
+
 
   return (
     <>
-      <Window />
-      uwu
+      <Window zindex={1} bringToFront={bringTofront}/>
+      <Window zindex={2} bringToFront={bringTofront}/>
     </>
   )
 }
--- src/applications/comp/Window.tsx
+++ src/applications/comp/Window.tsx
@@ -1,5 +1,5 @@
 import './Window.scss'
-import { MouseEventHandler, useState } from 'react'
+import { useState } from 'react'
 type iPos = {
   x:number,
   y:number
@@ -8,20 +8,26 @@ type iPos = {
 type WindowProps = {
   title: string,
   children: React.ReactNode,
-  initialPosition: iPos
+  initialPosition: iPos,
+  zindex: number,
+  bringToFront: ()=>number
 }
 
-const Window:React.FC<WindowProps> = ({title, children, initialPosition}) => {
+const Window:React.FC<WindowProps> = ({title, children, initialPosition, zindex, bringToFront}) => {
   const [position, setPosition] = useState<iPos>(initialPosition || {x:100, y:100})
   const [isDrag, setIsDrag] = useState<boolean>(false)
   const [offset, setOffset] = useState<iPos>({x:0, y:0})
+  const [zidnex, setZIndex] = useState<number>(0)
   const handleMouseDown = (e: React.MouseEvent) => {
     setIsDrag(true)
     setOffset({
       x: e.clientX - position.x,
       y: e.clientY - position.y
     })
+    setZIndex(bringToFront())
   }
+  // const handleClick = () => {
+  // }
   const handleMouseUp = () => {
     setIsDrag(false)
   }
@@ -33,17 +39,16 @@ const Window:React.FC<WindowProps> = ({title, children, initialPosition}) => {
       })
     }
   }
-
   return (
     <div
       className='window'
-      style={{left: `${position.x}px`, top:`${position.y}px`}}
+      style={{left: `${position.x}px`, top:`${position.y}px`, zIndex: zidnex, background: `${zindex===2? 'blue':'red'}`}}
       onMouseMove={handleMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
       onMouseDown={handleMouseDown}
     >
-
+      {children}
     </div>
   )
 }
